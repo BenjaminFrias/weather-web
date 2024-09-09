a7aa7e139eac4eda04fe0d268d0b2481f5f1ec7c
benjaminfrias <benjaminfrias2004@gmail.com>
2024-09-09 17:58:17 -0400

    add hourly forecast module

---
+++ assets/getHourlyForecast.js
@@ -0,0 +1,43 @@
+import { apiKey } from "./main.js";
+
+// Get hourly forecast data
+
+export async function getHourlyForecast(city) {
+	try {
+		const response = await fetch(
+			`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}`
+		);
+		const data = await response.json();
+
+		const currentHour = data.location.localtime.split(" ")[1].split(":")[0];
+		const hourlyForecastArray = data.forecast.forecastday[0].hour;
+
+		const nextFiveHoursForecast = {};
+
+		hourlyForecastArray.forEach((forecast) => {
+			const forecastHour = forecast.time.split(" ")[1].split(":")[0];
+
+			// Get at least the next five forecastf from current hour
+			const forecastLength = Object.keys(nextFiveHoursForecast).length;
+
+			if (forecastHour > currentHour && forecastLength < 5) {
+				const forecastIcon = forecast.condition.icon;
+
+				// Formatted hour to 12 hours format
+				const period = forecastHour >= 12 ? "PM" : "AM";
+				const formattedHour = `${forecastHour % 12 || 12} ${period}`;
+
+				nextFiveHoursForecast[forecastLength] = [
+					formattedHour,
+					forecastIcon,
+				];
+			}
+		});
+		// Formmated hour
+
+		return [nextFiveHoursForecast];
+	} catch (error) {
+		console.error("Error:", error);
+		throw error; // Re-throw the error for handling in main.js
+	}
+}
